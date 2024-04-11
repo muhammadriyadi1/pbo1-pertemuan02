bbfc80c4b9e2edb4246e2f6ff3016bc35d420b99
muhammadriyadi <riyadi1124543@gmail.com>
2024-04-11 15:57:01 +0800

    Compile & Run

--- InputOutput.java
+++ InputOutput.java
@@ -1,11 +1,11 @@
-import java.util.Scanner
+import java.util.Scanner;
 
 public class InputOutput{
-public static void main(String [] args)}
+public static void main(String [] args){
 Scanner scanner = new Scanner(System.in);
 String nama;
 System.out.print("Masukkan nama anda : ");
-nama = scanner.nextline();
+nama = scanner.nextLine();
 System.out.println("Hai, "+nama);
 }
 }
